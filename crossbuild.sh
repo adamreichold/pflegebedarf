5649bc1c23528243519d065a5f75dad87e549893
Adam Reichold <adam.reichold@t-online.de>
2022-09-28 21:40:18 +0200

    Optimize for specific target CPU during cross builds.

--- crossbuild.sh
+++ crossbuild.sh
@@ -2,6 +2,7 @@
 
 : "${ARCH:=arm64}"
 : "${TARGET:=aarch64-unknown-linux-gnu}"
+: "${TARGET_CPU:=cortex-a53}"
 : "${TARGET_CC:=aarch64-linux-gnu-gcc}"
 
 cargo check
@@ -16,7 +17,7 @@ apt-get install --yes --no-install-recommends crossbuild-essential-${ARCH} zlib1
 
 export CARGO_TARGET_DIR="target-crossbuild-${ARCH}"
 export PKG_CONFIG_ALLOW_CROSS=1
-export RUSTFLAGS="-C linker=${TARGET_CC}"
+export RUSTFLAGS="-Clinker=${TARGET_CC} -Ctarget-cpu=${TARGET_CPU}"
 export TARGET_CC="${TARGET_CC}"
 
 cd /src
